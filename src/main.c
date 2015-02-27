b9e12372593d986ad610eceec62be2e7d56a53c0
David Stosik <david.stosik+git-noreply@gmail.com>
2015-02-27 11:42:42 +0900

    First commit.

---
+++ src/main.c
@@ -0,0 +1,44 @@
+#include <pebble.h>
+
+static Window *window;
+static TextLayer *text_layer;
+
+
+
+
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+  text_layer_set_text(text_layer, "Press a button");
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
+
+static void window_unload(Window *window) {
+  text_layer_destroy(text_layer);
+}
+
+static void init(void) {
+  window = window_create();
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  window_stack_push(window, true);
+}
+
+static void deinit(void) {
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+
+  app_event_loop();
+  deinit();
+}
