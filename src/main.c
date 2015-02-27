5c657d81d7657d5890a27220221cdc24417547d4
David Stosik <david.stosik+git-noreply@gmail.com>
2015-02-27 14:38:06 +0900

    Change bg color on every minute, display time and color name.

--- src/main.c
+++ src/main.c
@@ -1,26 +1,77 @@
 #include <pebble.h>
+#include "color_names.h"
 
 static Window *window;
+static TextLayer *name_layer;
 static TextLayer *time_layer;
+static GFont font_gothic_18;
+static GFont font_roboto_49;
+
+static GColor8 get_color_from_minute(uint8_t minute) {
+  uint8_t argb = GColorBlackARGB8 + minute + 1;
+
+  if (argb >= GColorDarkGrayARGB8) { argb++; }
+  if (argb >= GColorLightGrayARGB8) { argb++; }
+
+  return (GColor8){ .argb = argb };
+}
+
+static GColor8 gcolor_fgcolor(GColor8 bgcolor) {
+  uint8_t r, g, b;
+  r = (bgcolor.argb & 0b110000) >> 4;
+  g = (bgcolor.argb & 0b001100) >> 2;
+  b = bgcolor.argb & 0b000011;
+  return r+g+b < 5 ? GColorWhite : GColorBlack;
+}
+
+static GColor8 gcolor_complementary(GColor8 color) {
+  return (GColor8){ argb: (0b00111111 ^ color.argb) };
+}
 
 void update_screen() {
   time_t now = time(NULL);
   struct tm * now_tm = localtime(&now);
 
-  text_layer_set_text(time_layer, "Press a button");
+  char *time_str = "00:00";
+  clock_copy_time_string(time_str, 6);
 
-  GColor8 bg = (GColor8){.argb = (0b11000000 + now_tm->tm_sec)};
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Updating screen, color: %d", bg.argb);
+  text_layer_set_text(name_layer, color_names[now_tm->tm_min]);
+  text_layer_set_text(time_layer, time_str);
 
+  GColor8 bg = get_color_from_minute(now_tm->tm_min);
   window_set_background_color(window, bg);
+
+  text_layer_set_text_color(name_layer, gcolor_fgcolor(bg));
+  text_layer_set_text_color(time_layer, gcolor_fgcolor(bg));
 }
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  time_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+  font_gothic_18 = fonts_get_system_font(FONT_KEY_GOTHIC_18);
+  font_roboto_49 = fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49);
+
+  uint8_t bottom_margin = 5;
+  uint8_t height = 20;
+
+  name_layer = text_layer_create((GRect) {
+    .origin = { 0, (bounds.size.h - height - bottom_margin) },
+    .size = { bounds.size.w, (height + bottom_margin) }
+  });
+  text_layer_set_text_alignment(name_layer, GTextAlignmentCenter);
+  text_layer_set_overflow_mode(name_layer, GTextOverflowModeWordWrap);
+  text_layer_set_font(name_layer, font_gothic_18);
+  text_layer_set_background_color(name_layer, GColorClear);
+  layer_add_child(window_layer, text_layer_get_layer(name_layer));
+
+  height = 70;
+  time_layer = text_layer_create((GRect) {
+    .origin = { 0, (bounds.size.h - height)/2 },
+    .size = { bounds.size.w, height }
+  });
   text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
+  text_layer_set_font(time_layer, font_roboto_49);
   text_layer_set_background_color(time_layer, GColorClear);
   layer_add_child(window_layer, text_layer_get_layer(time_layer));
 
@@ -35,6 +86,7 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 }
 
 static void window_unload(Window *window) {
+  text_layer_destroy(name_layer);
   text_layer_destroy(time_layer);
 }
 
@@ -46,7 +98,7 @@ static void init(void) {
   });
   window_stack_push(window, true);
 
-  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit(void) {
