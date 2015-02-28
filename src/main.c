6d3e0a15f286fb798c17b695ec0ac555b65cc305
David Stosik <david.stosik+git-noreply@gmail.com>
2015-02-28 19:13:02 +0900

    Improve time display, add date (inspired by Pebble's Symplicity).

--- src/main.c
+++ src/main.c
@@ -6,8 +6,10 @@
 static Window *window;
 static TextLayer *name_layer;
 static TextLayer *time_layer;
-static GFont font_gothic_18;
-static GFont font_roboto_49;
+static TextLayer *date_layer;
+static BitmapLayer *separator_layer;
+static char time_str[] = "00:00";
+static char date_str[] = "Xxxxxxxxxxxxxxxxxxxxxxx 00";
 
 static GColor8 get_color_from_minute(uint8_t minute) {
   uint8_t argb = GColorBlackARGB8 + minute + 1;
@@ -29,33 +31,38 @@ static GColor8 gcolor_fgcolor(GColor8 bgcolor) {
 }
 
 void update_screen(struct tm *tick_time) {
-  char *time_str = "00:00";
-  clock_copy_time_string(time_str, 6);
+  char *time_format = clock_is_24h_style() ? "%R" : "%I:%M";
+
+  strftime(time_str, sizeof(time_str), time_format, tick_time);
+  strftime(date_str, sizeof(date_str), "%B %e", tick_time);
 
   uint8_t color_index = DEBUG ? tick_time->tm_sec : tick_time->tm_min;
+
   text_layer_set_text(name_layer, color_names[color_index]);
   text_layer_set_text(time_layer, time_str);
+  text_layer_set_text(date_layer, date_str);
 
   GColor8 bg = get_color_from_minute(color_index);
   window_set_background_color(window, bg);
 
-  text_layer_set_text_color(name_layer, gcolor_fgcolor(bg));
-  text_layer_set_text_color(time_layer, gcolor_fgcolor(bg));
+  GColor fg_color = gcolor_fgcolor(bg);
+  text_layer_set_text_color(name_layer, fg_color);
+  text_layer_set_text_color(time_layer, fg_color);
+  text_layer_set_text_color(date_layer, fg_color);
+  bitmap_layer_set_background_color(separator_layer, fg_color);
 }
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  font_gothic_18 = fonts_get_system_font(FONT_KEY_GOTHIC_18);
-  font_roboto_49 = fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49);
-
-  uint8_t bottom_margin = 5;
-  uint8_t height = 20;
+  GFont font_gothic_18 = fonts_get_system_font(FONT_KEY_GOTHIC_18);
+  GFont font_gothic_24_bold = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
+  GFont font_roboto_49 = fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49);
 
   name_layer = text_layer_create((GRect) {
-    .origin = { 0, (bounds.size.h - height - bottom_margin) },
-    .size = { bounds.size.w, (height + bottom_margin) }
+    .origin = { 0, 0 },
+    .size = { bounds.size.w, 23 }
   });
   text_layer_set_text_alignment(name_layer, GTextAlignmentCenter);
   text_layer_set_overflow_mode(name_layer, GTextOverflowModeWordWrap);
@@ -63,15 +70,29 @@ static void window_load(Window *window) {
   text_layer_set_background_color(name_layer, GColorClear);
   layer_add_child(window_layer, text_layer_get_layer(name_layer));
 
-  height = 70;
   time_layer = text_layer_create((GRect) {
-    .origin = { 0, (bounds.size.h - height)/2 },
-    .size = { bounds.size.w, height }
+    .origin = { 0, bounds.size.h/2 + 10 },
+    .size = { bounds.size.w - 10, 50 }
   });
-  text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(time_layer, GTextAlignmentRight);
   text_layer_set_font(time_layer, font_roboto_49);
   text_layer_set_background_color(time_layer, GColorClear);
   layer_add_child(window_layer, text_layer_get_layer(time_layer));
+
+  date_layer = text_layer_create((GRect) {
+    .origin = { 10, bounds.size.h/2 - 19 },
+    .size = { bounds.size.w - 20, 31 }
+  });
+  text_layer_set_text_alignment(date_layer, GTextAlignmentLeft);
+  text_layer_set_font(date_layer, font_gothic_24_bold);
+  text_layer_set_background_color(date_layer, GColorClear);
+  layer_add_child(window_layer, text_layer_get_layer(date_layer));
+
+  separator_layer = bitmap_layer_create((GRect) {
+    .origin = { 10, bounds.size.h/2 + 12 },
+    .size = { bounds.size.w - 20, 2 }
+  });
+  layer_add_child(window_layer, bitmap_layer_get_layer(separator_layer));
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -84,6 +105,8 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void window_unload(Window *window) {
   text_layer_destroy(name_layer);
   text_layer_destroy(time_layer);
+  text_layer_destroy(date_layer);
+  bitmap_layer_destroy(separator_layer);
 }
 
 static void init(void) {
