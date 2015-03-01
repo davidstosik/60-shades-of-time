75c383c856e89ddaab563641a14eaa41a39c3026
David Stosik <david.stosik+git-noreply@gmail.com>
2015-03-01 18:23:11 +0900

    Make a very basic version for Aplite.

--- src/colorize.c
+++ src/colorize.c
@@ -1,5 +1,41 @@
 #include <pebble.h>
 
+#ifndef PBL_COLOR
+
+static char* color_names[] = {
+  "Black",
+  "White",
+};
+
+static GColor get_color_from_minute(uint8_t minute) {
+  return (GColor)(minute%2);
+}
+
+static GColor gcolor_fgcolor(GColor bgcolor) {
+  return 1 - bgcolor;
+}
+
+void colorize_screen(Window *window, uint8_t color_index) {
+  void** layers = window_get_user_data(window);
+  TextLayer *name_layer = layers[0];
+  TextLayer *time_layer = layers[1];
+  TextLayer *date_layer = layers[2];
+  BitmapLayer *separator_layer = layers[3];
+
+  text_layer_set_text(name_layer, color_names[color_index%2]);
+
+  GColor bg = get_color_from_minute(color_index);
+  window_set_background_color(window, bg);
+
+  GColor fg_color = gcolor_fgcolor(bg);
+  text_layer_set_text_color(name_layer, fg_color);
+  text_layer_set_text_color(time_layer, fg_color);
+  text_layer_set_text_color(date_layer, fg_color);
+  bitmap_layer_set_background_color(separator_layer, fg_color);
+}
+
+#else
+
 static char* color_names[] = {
   "Oxford Blue",
   "Duke Blue",
@@ -63,7 +99,7 @@ static char* color_names[] = {
   "Pastel Yellow",
 };
 
-GColor get_color_from_minute(uint8_t minute) {
+static GColor get_color_from_minute(uint8_t minute) {
   uint8_t argb = GColorBlackARGB8 + minute + 1;
 
   if (argb >= GColorDarkGrayARGB8) { argb++; }
@@ -72,7 +108,7 @@ GColor get_color_from_minute(uint8_t minute) {
   return (GColor8){ .argb = argb };
 }
 
-GColor gcolor_fgcolor(GColor bgcolor) {
+static GColor gcolor_fgcolor(GColor bgcolor) {
   // Inspiration: http://stackoverflow.com/a/946734/2341409
   uint8_t r, g, b;
   r = (bgcolor.argb & 0b110000) >> 4;
@@ -101,3 +137,4 @@ void colorize_screen(Window *window, uint8_t color_index) {
   bitmap_layer_set_background_color(separator_layer, fg_color);
 }
 
+#endif // ifndef PBL_COLOR
